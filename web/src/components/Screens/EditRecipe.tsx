4233911b8e00b4561ed11aff56c95e89d9eb2236
Fernan Cetina <fernancetina@Fernan.local>
2022-10-30 11:41:28 -0600

    feat: upload images work

--- web/src/components/Screens/EditRecipe.tsx
+++ web/src/components/Screens/EditRecipe.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react"
+import { Input } from "@mui/material";
+import { useEffect, useRef, useState } from "react"
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import { editRecipe } from "../../placeHolders/DashboardCards";
@@ -6,6 +7,18 @@ import { editRecipe } from "../../placeHolders/DashboardCards";
 //Componente que genera la vista de una receta en particular
 export const EditRecipe: React.FC = () => {
     const { id } = useParams();
+    const fileInput = useRef() as React.MutableRefObject<HTMLInputElement>;
+    const [fileName, setFileName] = useState("Nada ha sido seleccionado");
+
+    const onFilechange = ( e:React.ChangeEvent<HTMLInputElement> ) => {
+        /*Selected files data can be collected here.*/
+        const input = (e.target as HTMLInputElement).files![0];
+        setFileName( input? input.name : "Nada ha sido seleccionado" );
+    }
+    const onBtnClick = () => {
+        /*Collecting node-element and performing click*/
+        fileInput.current.click();
+    }
 
     useEffect(() => {
         
@@ -32,6 +45,7 @@ export const EditRecipe: React.FC = () => {
             costo: 0,
             ingredientes: '',
             pasos: '',
+            imagen: ''
         },
       });
     
@@ -45,7 +59,9 @@ export const EditRecipe: React.FC = () => {
     }
 
     const onSubmit = (data:any) => {
-        window.location.href = '/mis-recetas';
+        //window.location.href = '/mis-recetas';
+        console.log(data);
+        
     };
 
     return (
@@ -96,11 +112,24 @@ export const EditRecipe: React.FC = () => {
 
                 <h3> Instrucciones* </h3>
                 <textarea {...register("pasos", { required: true })} 
-                
                 autoComplete='off' className="form-control textArea" rows={15}/>
+
+                
+                <h3> Imagen* </h3>
+                <div className="d-flex justify-content-center">
+                    <input type="file" data-buttontext="hello" accept="image/*" 
+                    {...register("imagen", { required: true })} 
+                    onChange={(e)=>onFilechange(e)}
+                    className='form-control' style={{width:'250px',display:'none'}}
+                    ref={fileInput}
+                    />
+                    <button className='form-control' onClick={onBtnClick} style={{width:'170px'}}>Sube una imagen</button>
+                    <label className="labelForFile">{fileName}</label>
+                </div>
+
                 <br/>
 
-                <input id='buttonAdd' type="submit" value="Enviar" disabled={!isDirty}/>
+                <Input id='buttonAdd' type="submit" value="Enviar" disabled={!isDirty} style={{width:'80px'}}/>
                 </div>
 
             </form>
