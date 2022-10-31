b93696676e134ee31b758f542eb30e191dbe0de1
Fernan Cetina <fernancetina@Fernan.local>
2022-10-31 10:38:08 -0600

    fix: upload image bug

--- web/src/components/Screens/EditRecipe.tsx
+++ web/src/components/Screens/EditRecipe.tsx
@@ -9,27 +9,26 @@ export const EditRecipe: React.FC = () => {
     const { id } = useParams();
     const fileInput = useRef() as React.MutableRefObject<HTMLInputElement>;
     const [fileName, setFileName] = useState("Nada ha sido seleccionado");
+    const isRequired = id ? false : true;
 
     const onFilechange = ( e:React.ChangeEvent<HTMLInputElement> ) => {
-        /*Selected files data can be collected here.*/
         const input = (e.target as HTMLInputElement).files![0];
         setFileName( input? input.name : "Nada ha sido seleccionado" );
+        setValue('imagen', input.name);
     }
-    const onBtnClick = () => {
-        /*Collecting node-element and performing click*/
+    const onBtnClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        //e.preventDefault();
         fileInput.current.click();
     }
 
     useEffect(() => {
-        
         if(id){
-            setValue('titulo', editRecipe.titulo);
+            setValue('titulo', editRecipe.titulo, {shouldDirty: false});
             setValue('tiempo', editRecipe.tiempo);
             setValue('costo', editRecipe.costo);
             setValue('ingredientes', editRecipe.ingredientes);
             setValue('pasos', editRecipe.pasos);
         }
-
     }, []);
 
     const {
@@ -62,25 +61,28 @@ export const EditRecipe: React.FC = () => {
     const onSubmit = (e:React.FormEvent<HTMLFormElement>,data:any) => {
         e.preventDefault();
         window.location.href = '/mis-recetas';
+    };
+
+    const newSubmit = (data:any) => {
         console.log(data);
-        
+        window.location.href = '/mis-recetas';
     };
 
     return (
         <div style={{maxWidth:700,margin:'auto'}}>
-            <form onSubmit={(e)=>onSubmit(e,getValues())}>
+            <form onSubmit={handleSubmit(newSubmit) }>
 
                 <h1>{id? 'Editar Receta*' : 'Nueva Receta*'}</h1>
 
                 <div style={{marginTop:'20pt'}}>
                 <h3> Título* </h3>
-                <input {...register("titulo", { required: true })} autoComplete='off' className="form-control"/>
+                <input {...register("titulo", { required: isRequired })} autoComplete='off' className="form-control"/>
 
                 <div className="d-flex justify-content-between">
                     <div>
                         <h3 style={{textAlign:'start'}}> Tiempo* </h3>
                         <div className="d-flex">
-                            <input {...register("tiempo", { required: true, min:0, valueAsNumber: true})}
+                            <input {...register("tiempo", { required: isRequired, min:0, valueAsNumber: true})}
                             min={0}
                             onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('Solo se permiten numeros positvos, sin caracteres :)')}
                             onKeyDown={(e) => justNumber(e)}
@@ -94,7 +96,7 @@ export const EditRecipe: React.FC = () => {
                         <h3 style={{textAlign:'start'}}> Costo* </h3>
                         <div className="d-flex">
                             <p className="smallTagForInput">$</p>
-                            <input {...register("costo", { required: true, min:0, valueAsNumber: true})}
+                            <input {...register("costo", { required: isRequired, min:0, valueAsNumber: true})}
                             min={0}
 
                             onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('Solo se permiten numeros positvos, sin caracteres :)')}
@@ -120,12 +122,14 @@ export const EditRecipe: React.FC = () => {
                 <h3> Imagen* </h3>
                 <div className="d-flex justify-content-center">
                     <input type="file" data-buttontext="hello" accept="image/*" 
-                    {...register("imagen", { required: true })} 
+                    
                     onChange={(e)=>onFilechange(e)}
                     className='form-control' style={{width:'250px',display:'none'}}
                     ref={fileInput}
                     />
-                    <button className='form-control' onClick={onBtnClick} style={{width:'170px'}}>Sube una imagen</button>
+                    <button className='form-control' 
+                    {...register("imagen", { required: true })} 
+                    onClick={(e)=>onBtnClick(e)} style={{width:'170px'}}>Sube una imagen</button>
                     <label className="labelForFile">{fileName}</label>
                 </div>
 
