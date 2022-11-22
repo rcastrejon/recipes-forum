fb171a08780ed1a1e5d206784775e3a5cbcf0665
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2022-11-22 08:45:29 -0600

    increase max connections

--- server/app/redis.py
+++ server/app/redis.py
@@ -6,6 +6,7 @@ from app.core.config import get_settings
 @fastapi_plugins.registered_configuration
 class AppSettings(fastapi_plugins.RedisSettings):
     redis_url: str = get_settings().REDIS_URL
+    redis_max_connections: int = 50
 
 
 config = fastapi_plugins.get_config()
