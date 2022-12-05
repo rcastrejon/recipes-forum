db0a8f216fa01098328cefbbc8aa148838a42122
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2022-12-05 10:32:09 -0600

    chore: change api url

--- web/src/services/services.ts
+++ web/src/services/services.ts
@@ -15,7 +15,7 @@ export const postLogin = (body: any): Promise<Response> => {
     query.append('username',body.username);
     query.append('password',body.password);
     
-    return fetch('https://recipes-forum-api.onrender.com/login',{
+    return fetch('https://recipes-forum-production.up.railway.app/login',{
         body:query,
         method:'POST'
     }).then(
@@ -58,11 +58,11 @@ export const getLiveRecipes = (): Promise<Recipe[]> => get(`live`);
 export const notifyView = (id:Key): Promise<Response> => get(`recipes/${id}`);
 
 export const publishRecipe = async(recipe:any):Promise<Response> => {
-    return await fetch('https://recipes-forum-api.onrender.com/recipes',{
+    return await fetch('https://recipes-forum-production.up.railway.app/recipes',{
         method:'POST',
         headers:{
             "Authorization": 'Bearer ' + localStorage.getItem("security-token") || "",
-            "Access-Control-Allow-Origin": "https://recipes-forum-api.onrender.com/"
+            "Access-Control-Allow-Origin": "https://recipes-forum-production.up.railway.app/"
         },
         body: recipe
     });
